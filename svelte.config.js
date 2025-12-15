bbd61292e91d8cd6155323ce639dc28905d0c70a
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2025-12-15 03:34:13 -0500

    Github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,6 +12,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+		paths: {
+			base: process.env.BASE_PATH ?? ''
+		},
 		alias: {
 			$demo: 'src/demo'
 		}
